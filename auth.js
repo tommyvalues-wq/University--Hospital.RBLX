ccfa75c33d6623d1bcf3718d60a7e21506dec2e7
tommyvalues-wq <tommyvalues@gmail.com>
2026-06-06 03:40:52 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,20 @@
-// Put this in your website JS and change the URL after you deploy the backend.
-const API_BASE = 'https://https://uhh-backend.onrender.com';
+const API_BASE = "https://uhh-backend.onrender.com";
 
 function getToken() {
-  return localStorage.getItem('uhh_auth_token');
+  return localStorage.getItem("uhh_auth_token");
 }
 
 async function api(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
     ...options,
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       ...(getToken() ? { Authorization: `Bearer ${getToken()}` } : {}),
       ...(options.headers || {})
     }
   });
- const redirectUrl = new URL(
-  `${process.env.PUBLIC_SITE_URL}/staff-portal.html`
-);
+
+  return response.json();
 }
 
 function startRobloxLogin() {
